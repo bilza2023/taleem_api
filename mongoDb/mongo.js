52398e60b258012aa5d570cfaae027612efca1a8
Bilal Tariq <bilza2023@gmail.com>
2024-05-14 13:27:39 +0500

    cyclic.sh gone -- api moved to DO , cant login

--- mongoDb/mongo.js
+++ mongoDb/mongo.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 //.......................................................
-mongoose.connect( process.env.MONGO_URI , { useNewUrlParser: true});
+// mongoose.connect( process.env.MONGO_URI , { useNewUrlParser: true});
+mongoose.connect( "mongodb+srv://bilza2023:bils32611@skillzaadb.fswow27.mongodb.net/skillzaaDb" , { useNewUrlParser: true});
 // mongoose.connect( process.env.MONGO_DB_LOCAL_URL , { useNewUrlParser: true});
 
 const db = mongoose.connection;
